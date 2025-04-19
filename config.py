d9036ea856208f1a6533a6edf351d588b43ed562
ajwinkworth <a.j.winkworth@gmail.com>
2025-04-19 16:39:35 +0930

    Fix HX711 gain config: set GAIN to index 1 for 128 gain (CH_A_GAIN_128)

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ DOUT_PIN = 21  # Data pin
 SCK_PIN = 20   # Clock pin
 
 # HX711 Configuration
-GAIN = 128     # Default gain (128, 64, or 32)
+GAIN = 1  # Use 1 for 128 gain (CH_A_GAIN_128)
 
 # Scale Configuration
 SAMPLES = 3    # Number of samples to average for weight reading
